e8efda1ad90cb2798ac9385e3d863c5615138b90
plamenggg <plamen_ganchev_94@abv.bg>
2024-10-07 18:09:50 +0300

    Created the ListBox, Movie and MovieList compononents

---
+++ usePopcorn/src/components/MovieList.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { tempMovieData } from "../App";
+import Movie from "./Movie";
+
+function MovieList() {
+
+    const [movies, setMovies] = useState(tempMovieData);
+
+    return (
+        <ul className="list">
+              {movies?.map((movie) => (
+                <Movie movie={movie} key={movie.imdbID} />
+              ))}
+            </ul>
+    )
+}
+
+export default MovieList
